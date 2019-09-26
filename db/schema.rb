ee760c03a28daba4d91e5d9a9c663faca122f87c
vamsi <vamsikrishna3415@outlook.in>
2019-09-26 21:29:06 +0530

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_26_113450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "date"
